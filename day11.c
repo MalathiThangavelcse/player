e395e140000c0f112aba83659a076eeb33307389
MalathiThangavelcse <30829797+MalathiThangavelcse@users.noreply.github.com>
2018-02-02 14:02:22 +0530

    Create day11.c

---
+++ day11.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char a[20];
+scanf("%s",a);
+switch(s)
+{
+case "Monday":printf("\n NO");break;
+case "Tuesday":printf("\n NO");break;
+case "Wednesday":printf("\n NO");break;
+case "Thursday":printf("\n NO");break;
+case "Friday":printf("\n NO");break;
+case "Saturday":printf("\n YES");break;
+case "Sunday":printf("\nYES");break;
+default:printf("\n Enter day");
+}
+}
