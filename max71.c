ab78c67c03f2a136455816917f1911a255d781c9
MalathiThangavelcse <30829797+MalathiThangavelcse@users.noreply.github.com>
2018-02-09 14:47:54 +0530

    Create max71.c

---
+++ max71.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+int n,i,a[15];
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+    if(a[i]>a[i+1])
+    {
+        printf("%d\t",a[i]);
+    }
+    else
+    {
+        printf("%d\t",a[i+1]);
+    }
+}
+
+    return 0;
+}
+
+
+
